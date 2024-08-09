54d3f57a3804b8a2de563262de6a00b5f2f27040
Oscar <oscar15-91@hotmail.com>
2024-08-09 22:46:14 +0200

    Añadido main

--- src/main/java/dev/oscar/App.java
+++ src/main/java/dev/oscar/App.java
@@ -12,6 +12,33 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Tienda tienda = new Tienda("Informatica Pepin", "Pepin Alvarez", "123123123");
+        
+
+        Computador computer1 = new Computador("HP", 8, "Intel i3", "Windows 10", 150.00);
+        Computador computer2 = new Computador("Lenovo", 16, "Intel i5", "Windows 11", 250.00);
+        Computador computer3 = new Computador("Samsung", 8, "Intel i7", "Windows 11", 700.00);
+        Computador computer4 = new Computador("MSI", 8, "Intel i7", "Windows 11", 750.00);
+
+        tienda.agregarComputador(computer1);
+        tienda.agregarComputador(computer2);
+        tienda.agregarComputador(computer3);
+        tienda.agregarComputador(computer4);
+
+        tienda.listarComputadores();
+
+        System.out.println("Buscando computador de la marca Lenovo:");
+        Computador encontrado = tienda.buscarComputador("Lenovo");
+        if (encontrado != null) {
+            System.out.println(encontrado);
+        } else {
+            System.out.println("Computador no encontrado.");
+        }
+
+        System.out.println("\nEliminando computador marca Lenovo:");
+        tienda.eliminarComputador("Lenovo");
+
+        tienda.listarComputadores();
     }
-}
+    }
+
